26099393abdbe576b3d472dec2e2650a425c8710
PrasadPallaa <53461538+PrasadPallaa@users.noreply.github.com>
2020-09-24 13:19:12 +0530

    commit

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,18 @@
+import { environment } from './../../environments/environment';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { HttpClientModule } from '@angular/common/http';
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from './reducers';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    HttpClientModule,
+    StoreModule.forRoot(reducers, { metaReducers }),
+    !environment.production ? StoreDevtoolsModule.instrument() : [],
   ]
 })
 export class CoreModule { }
